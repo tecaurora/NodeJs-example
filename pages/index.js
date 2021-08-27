d418d1d858b43249d471ce62a50feb4d4df2ba17
joseGarciarodriguez <jose.garciarodriguez@uaem.edu.mx>
2021-08-27 11:09:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js ejemplo auto contruccion desde github!</a>
+          Welcome to <a href="https://nextjs.org">github Acctions Runing with docker registry!</a>
         </h1>
 
         <p className={styles.description}>
